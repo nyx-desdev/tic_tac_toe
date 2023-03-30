8f45d12ade43bfa4a9b9900dbc4c021b2670fa87
nyx-desdev <nyx.desdev@gmail.com>
2023-03-30 22:59:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const gameBoard = document.querySelector("#gameboard");
+const infoDisplay = document.querySelector("#info");
+
+let go = "circle";
+
+infoDisplay.textContent = "circle goes first";
+
+let gameBoardItems = ["", "", "", "", "", "", "", "", ""];
+
+function createBoard() {
+  gameBoardItems.forEach((_item, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", addGo);
+    gameBoard.appendChild(cellElement);
+  });
+}
+
+createBoard();
+
+function addGo(e) {
+  console.log(e.target);
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add(go);
+  e.target.appendChild(goDisplay);
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = "its is now " + go + "'s go";
+  e.target.removeEventListener("click", addGo);
+
+  checkScore();
+}
+
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square");
+  console.log(allSquares);
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  winningCombos.forEach((array) => {
+    const circleWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("circle")
+    );
+    if (circleWins) {
+      infoDisplay.textContent = "circle wins";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+  winningCombos.forEach((array) => {
+    const crossWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("cross")
+    );
+    if (crossWins) {
+      infoDisplay.textContent = "cross wins";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+}
